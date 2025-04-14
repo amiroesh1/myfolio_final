9a3e80b2b6102eddfa0c054dedab13ba755c316d
ansaritocode <ansar.sunkaruly@astanaboysbil.edu.kz>
2025-04-14 11:32:27 +0500

    Update route.ts

--- app/api/activities/route.ts
+++ app/api/activities/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
 import activities from '../../data/activities.json';
 
+// Add static configuration
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export async function GET() {
   return NextResponse.json(activities);
 }
